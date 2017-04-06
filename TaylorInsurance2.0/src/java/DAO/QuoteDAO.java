84bbf331909e9f63b51ebb8d99eabfd1f02ee0e4
Tim Bonnell <timbonnell@hotmail.com>
2017-04-06 16:48:57 -0230

    Merge origin/Tim-Dev into Tim-Dev
    Conflicts:
    	TaylorInsurance2.0/src/java/DAO/QuoteDAO.java

--- TaylorInsurance2.0/src/java/DAO/QuoteDAO.java
+++ TaylorInsurance2.0/src/java/DAO/QuoteDAO.java
@@ -33,7 +33,7 @@ import java.util.logging.Logger;
 public class QuoteDAO {
 
     public static HouseQuote createHouseQuote(Customer customer, House house) {
-        String sql = "{call insertHomeQuote (?,?,?)";
+        String sql = "{call insertHomeQuote (?,?,?)}";
         HouseQuote quote = new HouseQuote(null, null, null, customer, house);
         try (
                 Connection con = ConnectionManager.getConnection();
@@ -45,18 +45,19 @@ public class QuoteDAO {
             stm.setInt(1, Integer.parseInt(customer.getId()));
             stm.setInt(2, Integer.parseInt(house.getHouseId()));
             stm.setDouble(3, quote.getTotalPremium());
-
-            ResultSet rs = stm.executeQuery();
+            System.out.println("SP setup correct  hq:" + house.getHouseId() + " cid: " + customer.getId());
+            // ResultSet rs = stm.executeQuery();
             //Checks to see if house id comes back (house was stored successfully)
-            if (!rs.next()) {
-                throw new SQLException("No quote was created.");
-            } // If username and password are correct, set client to valid and set up the client
-            else {
-                System.out.println("House Quote ID: " + rs.getString(1));
-                quote.setId(rs.getString("policy_id"));
+
+            boolean hasMoreResultSets = stm.execute();
+
+            while (hasMoreResultSets) {
+                ResultSet rs = stm.getResultSet();
+                System.out.println("Got here");
+                quote.setId(rs.getString("quote_id"));
                 quote.setCreationDate(rs.getDate("creation_date").toLocalDate());
                 quote.setExpiryDate(rs.getDate("expiration_date").toLocalDate());
-            }
+            } // while has more rs
 
         } catch (NumberFormatException | SQLException ex) {
             System.out.println("Insert House Quote Failed: An Exception has occurred! " + ex);
@@ -66,7 +67,7 @@ public class QuoteDAO {
     }
 
     public static VehicleQuote createVehicleQuote(Customer customer, Vehicle vehicle) {
-        String sql = "{call insertAutoQuote(?,?,?)";
+        String sql = "{call insertAutoQuote(?,?,?)}";
         VehicleQuote quote = new VehicleQuote(null, null, null, customer, vehicle);
         try (
                 Connection con = ConnectionManager.getConnection();
@@ -80,19 +81,18 @@ public class QuoteDAO {
             stm.setInt(1, Integer.parseInt(customer.getId()));
             stm.setString(2, vehicle.getVehicleId());
             stm.setDouble(3, quote.getTotalPremium());
-            
+
             ResultSet rs = stm.executeQuery();
             //Checks to see if house id comes back (house was stored successfully)
             if (!rs.next()) {
                 throw new SQLException("Vehicle quote not created.");
             } // If username and password are correct, set client to valid and set up the client
             else {
-                System.out.println("Vehicle Quote ID: " + rs.getString(1));
-                quote.setId(rs.getString("policy_id"));
+                quote.setId(rs.getString("quote_id"));
                 quote.setCreationDate(rs.getDate("creation_date").toLocalDate());
                 quote.setExpiryDate(rs.getDate("expiry_date").toLocalDate());
             }
-            
+
         } catch (NumberFormatException | SQLException ex) {
             System.out.println("Insert Vehicle Quote Failed: An Exception has occurred! " + ex);
             quote = null;
@@ -106,10 +106,10 @@ public class QuoteDAO {
         try (
                 Connection con = ConnectionManager.getConnection();
                 CallableStatement stm = con.prepareCall(sql)) {
-            
+
             stm.setInt(1, Integer.parseInt(customer.getId()));
             ResultSet rs = stm.executeQuery();
-            
+
             while (rs.next()) {
                 House house = houseList.get(rs.getString("house_id"));
                 if (house == null) {
@@ -117,24 +117,24 @@ public class QuoteDAO {
                 }
                 quoteList.add(new HouseQuote(rs.getString("quote_id"), rs.getDate("creation_date").toLocalDate(), rs.getDate("expiry_date").toLocalDate(), customer, house));
             }
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(QuoteDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return quoteList;
     }
-    
+
     public static List<VehicleQuote> getVehicleQuotesByCustomerId(Customer customer, Map<String, Vehicle> vehicleList) {
         String sql = "{call getVehicleQuotesByCustomerId(?)}";
         List<VehicleQuote> quoteList = new ArrayList<>();
         try (
                 Connection con = ConnectionManager.getConnection();
                 CallableStatement stm = con.prepareCall(sql)) {
-            
+
             stm.setInt(1, Integer.parseInt(customer.getId()));
             ResultSet rs = stm.executeQuery();
-            
+
             while (rs.next()) {
                 Vehicle vehicle = vehicleList.get(rs.getString("house_id"));
                 if (vehicle == null) {
@@ -142,11 +142,11 @@ public class QuoteDAO {
                 }
                 quoteList.add(new VehicleQuote(rs.getString("quote_id"), rs.getDate("creation_date").toLocalDate(), rs.getDate("expiry_date").toLocalDate(), customer, vehicle));
             }
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(QuoteDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return quoteList;
     }
 }
